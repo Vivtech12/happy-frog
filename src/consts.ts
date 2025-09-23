c7b29060a3e060dc842743cf4e6fa74a08f3682e
unknown <joeyk2k@gmail.com>
2025-09-23 21:34:29 +0900

    branding

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Site Title";
-export const SITE_DESCRIPTION = "Very Descritive Title";
+export const SITE_TITLE = "Happy Frog";
+export const SITE_DESCRIPTION = "Happy Frog is a leading UK B2B wholesale distributor of consumer electronics, based in Birmingham, UK, serving clients across the UK";
